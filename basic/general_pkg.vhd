a76fc67c8a380264ffb170f3a5b9544a8ebd53b2
QuantumRipple <darkrunes13@gmail.com>
2017-01-04 18:24:13 -0600

    Minor comment and assert message changes

--- basic/general_pkg.vhd
+++ basic/general_pkg.vhd
@@ -21,8 +21,8 @@ end general_pkg;
 package body general_pkg is
    function f_log2_ceil (a : in integer := 1) return integer is
    begin
-      assert (a <= 2147483647) report "log2_ceil argument too large" severity error;
-      assert (a > 0) report "log2_ceil argument too small" severity error;
+      assert (a <= 2147483647) report "f_log2_ceil argument too large" severity error;
+      assert (a > 0) report "f_log2_ceil argument too small" severity error;
       for i in 0 to 30 loop
          if a <= 2**i then 
             return i; 
@@ -39,7 +39,7 @@ package body general_pkg is
    function f_min_integer (a : t_integer_vec) return integer is
       variable temp : integer;
    begin
-      assert a'length > 0 report "min_integer null list" severity error;
+      assert a'length > 0 report "f_min_integer null list" severity error;
       temp := a(0);
       for i in a'range loop
          if a(i) < temp then
@@ -52,7 +52,7 @@ package body general_pkg is
    function f_max_integer (a : t_integer_vec) return integer is
       variable temp : integer;
    begin
-      assert a'length > 0 report "max_integer null list" severity error;
+      assert a'length > 0 report "f_max_integer null list" severity error;
       temp := a(0);
       for i in a'range loop
          if a(i) > temp then
@@ -65,7 +65,7 @@ package body general_pkg is
    function f_gray_to_bin (a : std_logic_vector) return unsigned is
       variable temp : unsigned(a'range);
    begin
-      assert not a'ascending report "gray_to_bin incorrect direction" severity error;
+      assert not a'ascending report "f_gray_to_bin incorrect direction" severity error;
       temp(a'high) := a(a'high);
       for i in a'high-1 downto a'low loop
          temp(i) := a(i) xor temp(i+1); --note the long path of n-1 layers of xor. wide gray counters are slow to convert back to binary
@@ -77,7 +77,7 @@ package body general_pkg is
    function f_bin_to_gray (a : unsigned) return std_logic_vector is
       variable temp : std_logic_vector(a'range);
    begin
-      assert not a'ascending report "bin_to_gray incorrect direction" severity error;
+      assert not a'ascending report "f_bin_to_gray incorrect direction" severity error;
       temp(a'high) := a(a'high);
       for i in a'high-1 downto a'low loop
          temp(i) := a(i) xor a(i+1);
@@ -91,7 +91,7 @@ package body general_pkg is
       return f_bin_to_gray(f_gray_to_bin(a)+1);
    end f_gray_next;
    
-   function f_gray_rom (n : positive) return t_slv_vec is
+   function f_gray_rom (n : positive) return t_slv_vec is --consumes 3 LUTs at n=5, LUTs at n=6, 1 BRAM18 at n=10, 1 BRAM36 at n=11. Using math instead of ROMs is probably cheaper around n=8+
       variable temp : t_slv_vec(0 to 2**n-1)(n-1 downto 0);
    begin
       for i in temp'range loop
